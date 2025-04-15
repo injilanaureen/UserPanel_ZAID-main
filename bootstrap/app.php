ed8c3a36a68feee819dc280cea5d552c5ee6974a
Zaid <zaidiqbaliqbal28@gmail.com>
2025-04-15 15:47:56 +0530

    csrf7

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -21,12 +21,12 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
         // Adding CheckUserBalance to API middleware group (optional)
         $middleware->api(append: [
-            \App\Http\Middleware\CheckUserBalance::class,
+            // \App\Http\Middleware\CheckUserBalance::class,
             \App\Http\Middleware\CheckIpWhitelist::class,
         ]);
             $middleware->alias([
             "LocationCapture" => App\Http\Middleware\LocationCapture::class,
-            "check.balance" => \App\Http\Middleware\CheckUserBalance::class,
+            // "check.balance" => \App\Http\Middleware\CheckUserBalance::class,
             'ip.whitelist' => \App\Http\Middleware\CheckIpWhitelist::class,
             "auth" => Authenticate::class,
             'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
